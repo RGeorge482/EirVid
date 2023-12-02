f32c15953bcee57923bd95bbd47ac42847aeb70e
bergandara <gandaraber@gmail.com>
2023-12-02 03:39:40 +0000

    Constructor() and createDatabase() methods created. Necessary Packages imported

--- SchemaCreator.java
+++ SchemaCreator.java
@@ -3,11 +3,26 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package visionvibe;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  *
  * @author bernardogandara
  */
 public class SchemaCreator {
+    private String databaseName;
+    private DatabaseConnector connector;
+    
+    public SchemaCreator(String databaseName, DatabaseConnector connector){
+        this.databaseName = databaseName;
+        this.connector = connector;
+    }
+    
+    public void createDatabase(){
+        String sql = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+        
+    }
     
 }

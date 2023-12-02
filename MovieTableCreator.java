fa43021719f9c1263ead1d728385739460f1c527
bergandara <gandaraber@gmail.com>
2023-12-02 04:14:08 +0000

    Constructor and createMoviesTable created

--- MovieTableCreator.java
+++ MovieTableCreator.java
@@ -15,4 +15,27 @@ public class MovieTableCreator {
     private DatabaseConnector connector;
     private String databaseName;
     
+    //CONSTRUCTOR
+    public void MovieTableCreator(String databaseName, DatabaseConnector connector){
+        this.connector = connector;
+        this.databaseName = databaseName;
+    }
+    
+    public void createMoviesTable(){
+        String sql = "CREATE TABLE IF NOT EXISTS " + databaseName + ".Movies (" +
+                "MovieID INT AUTO_INCREMENT PRIMARY KEY, " +
+                "Title VARCHAR(255) NOT NULL, " +
+                "Category VARCHAR(255) NOT NULL, " + 
+                "Price DECIMAL(10, 2) NOT NULL" +
+                ")";
+        
+        try(Connection conn = connector.getConnection(); Statement stmt = conn.createStatement()){
+            stmt.executeUpdate(sql);
+            System.out.println("Movie table created successfully.");
+        }catch(SQLException e){
+            System.out.println("An error occured while creating the Movies table: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    
 }

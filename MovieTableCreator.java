e12c29566de7b652ac3b12c1750bf063ae7c8c62
bergandara <gandaraber@gmail.com>
2023-12-02 04:10:00 +0000

    MovieTableCreator class created with corresponding packages and attributes

---
+++ MovieTableCreator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package visionvibe;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author bernardogandara
+ */
+public class MovieTableCreator {
+    private DatabaseConnector connector;
+    private String databaseName;
+    
+}

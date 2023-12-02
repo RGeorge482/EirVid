ef271d249c1599b36f0c4327ff0b258ba1bcaeb6
bergandara <gandaraber@gmail.com>
2023-12-02 03:55:13 +0000

    userTableCreator class created --> responsible for creating the user table in the DB

---
+++ UserTableCreator.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package visionvibe;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author bernardogandara
+ */
+public class UserTableCreator {
+    
+}

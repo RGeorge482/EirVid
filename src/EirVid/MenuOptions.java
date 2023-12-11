d628de23819f7bfdde9d83a5c3e08aaf4fe68edb
RGeorge482 <ratagheorghita@yahoo.com>
2023-12-11 05:36:19 +0000

    Create MenuOptions

---
+++ src/EirVid/MenuOptions.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EirVid;
+
+/**
+ *
+ * @author George
+ */
+public class MenuOptions {
+    public void user_menu_options(){
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("   You are logged in");
+        System.out.println("------------------------------------");
+        System.out.println("[1] Rent a movie");
+        System.out.println("[2] Check rented movies");
+        System.out.println("[3] Top 5 most recommended movies");
+        System.out.println("[4] Modify Your Profile");
+        System.out.println("------------------------------------");
+        System.out.println("");
+    }
+
+    public void user_status(){
+        System.out.println("");
+        System.out.println("------------------------------------");
+        System.out.println("\t\tUser");
+        System.out.println("------------------------------------");
+        System.out.println("[1] Register");
+        System.out.println("[2] Log In");
+        System.out.println("");
+    }
+}

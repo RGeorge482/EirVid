1f69e20a44bc4eed9d9e302148db2e3961e3bd6a
RGeorge482 <ratagheorghita@yahoo.com>
2023-12-11 12:31:13 +0000

    Main class update

--- src/EirVid/EirVid.java
+++ src/EirVid/EirVid.java
@@ -15,9 +15,6 @@ import java.sql.SQLException;
  */
 public class EirVid {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException {
         
         

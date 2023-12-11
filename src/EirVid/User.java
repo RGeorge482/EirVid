03bb94177fd8890b76bfeae120325eb0a2e8b4e1
RGeorge482 <ratagheorghita@yahoo.com>
2023-12-11 05:47:55 +0000

    Update User with register and login methods

--- src/EirVid/User.java
+++ src/EirVid/User.java
@@ -51,4 +51,17 @@ public class User {
     public String toString() {
         return "User{" + "user_name=" + user_name + ", email=" + email + ", password=" + password + ", user_ID=" + user_ID + '}';
     }
+    
+    
+    //  Register 
+    public boolean register(User user) {
+        // connect to the database to register
+        return true;
+    }
+    
+    // Login
+     public boolean user_login(String name, String user_password) {
+        // connect to database to login
+        return true;
+    }
 }

5e53c82f5f0040a642fccad11920e6bc1354eae2
RGeorge482 <ratagheorghita@yahoo.com>
2023-12-11 05:39:56 +0000

    Create UserInterface

---
+++ src/EirVid/UserInterface.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EirVid;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author George
+ */
+public interface UserInterface {
+    public boolean user_login(String name, String user_password) throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException;
+
+    public boolean register(User user) throws ClassNotFoundException, InstantiationException, IllegalAccessException;
+
+   // public String update_userinfo(String columnToBeChanged, String user_name, String email_address, String old_info, String new_info) throws ClassNotFoundException, InstantiationException, IllegalAccessException; 
+}
